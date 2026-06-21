977c388cb010bc401ddb55f8fadf3bd5f051abd9
katjes <katjes733@gmx.net>
2026-06-21 10:18:55 -0700

    fix: session cookie upgrade to strict

--- src/server/main.ts
+++ src/server/main.ts
@@ -54,7 +54,7 @@ app.use(
     cookie: {
       httpOnly: true,
       secure: process.env.SSL_ENABLED === "true",
-      sameSite: "lax",
+      sameSite: "strict",
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     },
   }),
